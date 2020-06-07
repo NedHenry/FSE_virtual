a28072b4f010a8a16cd498e0dbe8fce37d4cea8a
Ned Henry <ethenry@ucsc.edu>
2020-06-07 12:29:17 -0700

    install missing gems before launching webpacker server

--- scripts/start_webpack_dev.sh
+++ scripts/start_webpack_dev.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-echo "starting webpack dev" && export NODE_OPTIONS="--max_old_space_size=4096" && yarn && rm -rf /fse_virtual/public/packs && /fse_virtual/bin/webpack-dev-server
+echo "starting webpack dev"
+export NODE_OPTIONS="--max_old_space_size=4096"
+yarn
+cd /fse_virtual
+bundle install
+rm -rf /fse_virtual/public/packs
+/fse_virtual/bin/webpack-dev-server
