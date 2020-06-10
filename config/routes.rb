78d248af051a95dfe3729d58f2e8ae9d508b3f99
Ned Henry <ethenry@ucsc.edu>
2020-06-10 10:46:04 -0700

    activate ActionCable for websockets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  
   devise_for :users
   get 'welcome', to: "welcome#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
