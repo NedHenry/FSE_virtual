411b40c178b8637788c2ded342eb3516d7dd1b0e
Ned Henry <ethenry@ucsc.edu>
2020-06-24 14:37:43 -0700

    when creating a new page, add default text to default view partial

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,7 @@ class Page < ApplicationRecord
   def save
     slug = title.parameterize if slug.nil?
     unless File.file?(view_partial_path)
-      File.open(view_partial_path, "w") {}
+      File.open(view_partial_path, "w") {|f| f.write('<%= html.html_safe %>')}
     end
     super
   end
@@ -19,5 +19,5 @@ class Page < ApplicationRecord
   def view_partial_path
     File.join(Rails.root,"app/views/pages",view_partial_filename)
   end
-  
+
 end
