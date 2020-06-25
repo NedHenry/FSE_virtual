a9f7aa605a11f29d49a2b336dae784f986a6bad1
Ned Henry <ethenry@ucsc.edu>
2020-06-24 21:36:27 -0700

    comment out ability settings for models not yet implemented

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,39 +6,38 @@ class Ability
   def initialize(user)
 
     if user.present?
-      can :read, Page
-      can :read, Endorsement
-      can :read, Link
+      can :show, Page
+      can :show, Endorsement
 
-      can :read, Event
+#      can :read, Event
 
-      if user.moderator?
-        can :manage, Room do |room|
-          room.moderator_ids.include? user.id
-        end
-      end
+#     if user.moderator?
+#       can :manage, Room do |room|
+#         room.moderator_ids.include? user.id
+#       end
+#     end
 
-      if user.affiliate?
-        can :manage, Affiliate, {user_id: user.id}
-	can :manage, Event, {user_id: user.id}
-	can :manage, Room, {owner_id: user.id}
-	can :create, Event
-      end
+#      if user.affiliate?
+#       can :manage, Affiliate, {user_id: user.id}
+#	can :manage, Event, {user_id: user.id}
+#	can :manage, Room, {owner_id: user.id}
+#	can :create, Event
+#      end
 
-      if user.performer?
-        can :manage, Event, {user_id: user.id}
-        can :manage, Room, {owner_id:  user.id}
-        can :create, Event
-      end
+#      if user.performer?
+#        can :manage, Event, {user_id: user.id}
+#        can :manage, Room, {owner_id:  user.id}
+#        can :create, Event
+#      end
 
       if user.admin?
 	can :manage, Page
 	can :manage, Affiliate
         can :manage, Endorsement
 
-	can :manage, Room
-	can :manage, Link
-	can :manage, Event
+#	can :manage, Room
+#	can :manage, Link
+#	can :manage, Event
       end
     end
   end
