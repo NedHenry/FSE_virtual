9b13d1d0fee45784d4c6b715c8b04744197d0809
Ned Henry <ethenry@ucsc.edu>
2020-06-26 23:12:52 -0700

    fix flash classes and add clashfinder name helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
 
   def flash_class(level)
-    case level
+    case level.to_sym
         when :notice then "alert alert-info"
         when :success then "alert alert-success"
         when :error then "alert alert-error"
         when :alert then "alert alert-error"
     end
+  end
+
+  def clashfinder_name
+    "test"
   end
 
   def resource_name
