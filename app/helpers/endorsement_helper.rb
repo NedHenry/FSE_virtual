5581adf95d6f3b1b31077abd9c571779574e19fb
Edward Henry <ethenry@ucsc.edu>
2020-07-23 16:59:41 -0700

    create separate helpers to pull different endorsement types

--- app/helpers/endorsement_helper.rb
+++ app/helpers/endorsement_helper.rb
@@ -1,7 +1,12 @@
 module EndorsementHelper
-  def random_endorsements num
-    return @endorsements if @endorsements.present?
-    num = [num, Endorsement.all.count].min
-    @endorsements = ( Endorsement.limit(num).order(Arel.sql('RAND()')) || [] )
+  def random_sponsors num
+    sponsors = Endorsement.where(endorsement_type: "sponsor")
+    num = [num, sponsors.count].min
+    sponsors.limit(num).order(Arel.sql('RAND()')) || []
   end
+
+  def cruise_spaces
+    Endorsement.where(endorsement_type: "cruise").limit(6)
+  end
+  
 end
