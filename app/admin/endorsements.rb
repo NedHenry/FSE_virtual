474c07e155d510dd851cb111f0bda3e62d26c2fc
Edward Henry <ethenry@ucsc.edu>
2020-07-23 16:58:28 -0700

    add new columns to endorsements to weight by contribution

--- app/admin/endorsements.rb
+++ app/admin/endorsements.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Endorsement do
-  permit_params :affiliate_id,:name,:caption,:enabled,:media_uri,:media_type, :banner
+  permit_params :affiliate_id,:name,:caption,:enabled,:media_uri,:media_type, :banner, :endorsement_type, :weight
 
   index do
     selectable_column
@@ -7,6 +7,8 @@ ActiveAdmin.register Endorsement do
     column :name
     column :caption
     column :enabled
+    column :endorsement_type
+    column :weight
     column :link
     column :media_uri
     column :media_type
@@ -20,6 +22,8 @@ ActiveAdmin.register Endorsement do
       row :caption
       row :link
       row :enabled
+      row :endorsement_type
+      row :weight
       row :banner do |e10t|
         image_tag url_for(e10t.banner.variant(resize: "200x300>"))
       end
@@ -38,6 +42,8 @@ ActiveAdmin.register Endorsement do
       f.input :link
       f.input :enabled, as: :boolean
       f.input :affiliate_id
+      f.input :endorsement_type
+      f.input :weight
       f.input :media_uri
       f.input :media_type
       f.input :banner, :as => :file
