e9553f5a54417f253605d5c0fe40dba4126f31e6
Edward Henry <ethenry@ucsc.edu>
2020-07-22 22:34:58 -0700

    fix customizing number of endorsements

--- app/helpers/endorsement_helper.rb
+++ app/helpers/endorsement_helper.rb
@@ -2,6 +2,6 @@ module EndorsementHelper
   def random_endorsements num
     return @endorsements if @endorsements.present?
     num = [num, Endorsement.all.count].min
-    @endorsements = ( Endorsement.limit(6).order(Arel.sql('RAND()')) || [] )
+    @endorsements = ( Endorsement.limit(num).order(Arel.sql('RAND()')) || [] )
   end
 end
