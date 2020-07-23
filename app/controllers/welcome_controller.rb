72c4ffe009abddd8e88c46faf63c34679f9c8c56
Edward Henry <ethenry@ucsc.edu>
2020-07-23 16:57:53 -0700

    add separate rules page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,14 @@ class WelcomeController < ApplicationController
   layout :welcome_layout
 
   def welcome_layout
-    params['demo']="show" ? "application" : "splash"
+    params['demo']=="show" ? "application" : "splash"
   end
 
   skip_before_action :verify_age
 
+  def rules
+  end
+  
   def show
   #  @login_choice_text = "Hey there. First time?"
   end
