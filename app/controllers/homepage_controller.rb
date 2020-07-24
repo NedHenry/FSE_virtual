664de5f5d8ed337f5394a68c1e5305f8f1e34e4d
Edward Henry <ethenry@ucsc.edu>
2020-07-23 17:02:17 -0700

    fix conditional flash page display, but disable it for staging

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 class HomepageController < ApplicationController
   layout "iframer"
   def show
-    redirect_to :controller => 'welcome', :action => 'show' unless (user_signed_in? || params["demo"]="show")
+#    redirect_to :controller => 'welcome', :action => 'show' unless (user_signed_in? || params["demo"]=="show")
   end
   
 end
