029c69c37a392fc8b7c0d5660c26ebdd3feb3d61
Edward Henry <ethenry@ucsc.edu>
2020-09-23 13:47:27 -0700

    deploy to new ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "ec2-52-52-165-178.us-west-1.compute.amazonaws.com", user: "folsom", roles: %w{app db web}, my_property: :my_value
+server "ec2-54-176-7-83.us-west-1.compute.amazonaws.com", user: "folsom", roles: %w{app db web}, my_property: :my_value
 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
